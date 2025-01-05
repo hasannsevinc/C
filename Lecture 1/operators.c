e6e910349e412ea96646bf6e751fef6802460168
Hasan Sevinç <86223218+hasannsevinc@users.noreply.github.com>
2025-01-05 14:18:09 +0300

    update

---
+++ Lecture 1/operators.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1, b = 2, c = 3;
+    printf("Arithmetic Operators\n");
+    printf("%d\n", a + b); // 3
+    printf("%d\n", a - b); // -1
+    printf("%d\n", a * b); // 2
+    printf("%d\n", a / b); // 0
+    printf("%d\n", a % b); // 1
+
+    printf("Relational Operators\n"); // Output: 0  or 1
+    printf("%d\n", a == b); // 0
+    printf("%d\n", a != b); // 1
+    printf("%d\n", a > b);  // 0
+    printf("%d\n", a < b);  // 1
+    printf("%d\n", a >= b); // 0
+    printf("%d\n", a <= b); // 1
+    
+    printf("Logical Operators\n"); // Output: 0  or 1
+    printf("%d\n", a && b); // 1
+    printf("%d\n", a || b); // 1
+    printf("%d\n", !a);     // 0
+    
+    printf("Bitwise Operators\n"); // Bitwise operators work on bits and perform bit-by-bit operation.
+    printf("a & b : %d\n", a & b);  // 0
+    printf("%d\n", a | b);  // 3
+    printf("%d\n", a ^ b);  // 3
+    printf("%d\n", ~a);     // -2
+    printf("%d\n", a << b); // 4
+    printf("%d\n", a >> b); // 0
+    
+    printf("Increment and Decrement Operators\n");
+    printf("a++ : %d\n", a++); // 1
+    printf("a : %d\n", a);     // 2
+    printf("++a : %d\n", ++a); // 3
+
+    return 0;
+}
+
+// Operators in C:
+/*
+
+• Arithmetic operators: +, -, *, /, % (modulus)
+• Relational operators: ==, !=, >, <, >=, <=
+• Logical operators: && (AND), || (OR), ! (NOT)
+• Bitwise operators: &, |, ^, ~, <<, >>
+
+-- Logical Operators:
+• && (AND): Returns true if both the operands are true
+0 && 0 = 0
+0 && 1 = 0
+1 && 0 = 0
+1 && 1 = 1
+
+• || (OR): Returns true if one of the operands is true
+0 || 0 = 0
+0 || 1 = 1
+1 || 0 = 1
+1 || 1 = 1
+
+• ! (NOT): Returns true if the operand is false
+!0 = 1
+!1 = 0
+
+-- Bitwise Operators:
+• & (AND): Bitwise AND
+1 & 1 = 1
+1 & 0 = 0
+0 & 1 = 0
+0 & 0 = 0
+
+• | (OR): Bitwise OR
+1 | 1 = 1
+1 | 0 = 1
+0 | 1 = 1
+0 | 0 = 0
+
+• ^ (XOR): Bitwise XOR
+1 ^ 1 = 0
+1 ^ 0 = 1
+0 ^ 1 = 1
+0 ^ 0 = 0
+
+• ~ (NOT): Bitwise NOT
+~1 = 0
+~0 = 1
+
+• << (left shift): Shifts the bits of the number to the left
+1 << 2  = (100) : 4
+
+• >> (right shift): Shifts the bits of the number to the right
+1 >> 2  = (0) : 0
+4 >> 1 = (10) : 2
+
+Decrement and Increment Operators:
+• ++: Increment operator
+• --: Decrement operator
+
+5++ = 6 ( first use then increment)
+++5 = 6 ( first increment then use)
+
+5-- = 4 ( first use then decrement)
+--5 = 4 ( first decrement then use)
+
+*/
